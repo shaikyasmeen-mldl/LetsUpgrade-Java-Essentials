8b701233cf1a8c54a4b7e5e39b3a8bac24aeaa7a
Shaik Yasmeen <66788030+shaikyasmeen-mldl@users.noreply.github.com>
2020-11-01 16:59:58 +0530

    Add files via upload

---
+++ Assignment3_Day4/AvengersMain.java
@@ -0,0 +1,22 @@
+package com.myjava;
+
+public class AvengersMain {
+    public static void main(String[] args) {
+
+        Avengers[] avgs = new Avengers[5];
+
+        System.out.println("Avengers:");
+        for (int i=0;i< avgs.length;i++){
+            avgs[i] = new Avengers();
+            avgs[i].getDetails();
+            System.out.println();
+        }
+        System.out.println("Avengers display...");
+        for (int i=0;i< avgs.length;i++){
+            System.out.println();
+            avgs[i].displayDetails();
+            System.out.println();
+        }
+
+    }
+}
